22eebfecba1a58184eede524780d5305c18c1f41
KBAN-debug <kban4059@gmail.com>
2025-07-24 08:44:11 +0000

    update

--- api/server.js
+++ api/server.js
@@ -11,6 +11,7 @@ const { sendOtpEmail } = require('../GmailMailer.js');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const serverless = require('serverless-http');
+const io = new Server(server);
 const upload = multer({ 
   storage: multer.memoryStorage(),
   limits: {
@@ -40,7 +41,11 @@ app.use(session({
   saveUninitialized: true
 }));
 
+app.locals.io = io;
 
+io.on('connection', socket => {
+  console.log('🔌 Socket connected:', socket.id);
+});
 // Create tables if not exist
 async function initTables() {
   const sql = {
